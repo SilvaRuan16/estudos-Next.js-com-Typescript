7f9d2a459dbf32a202e5b4950b366314685b0759
SilvaRuan16 <ruan.gustavo.soares16@gmail.com>
2025-04-29 18:30:56 -0300

    Aula 5 - Page Not Found

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,10 +1,15 @@
+import { notFound } from "next/navigation"
+
 interface InfoParams {
   params: {
-    productId: number
+    productId: string
   }
 }
 
 export default function Info({ params }: InfoParams) {
+
+  if (parseInt(params.productId) > 100) notFound()
+
   return (
     <div>
       <p>Aqui fala sobre o produto {params.productId}</p>
