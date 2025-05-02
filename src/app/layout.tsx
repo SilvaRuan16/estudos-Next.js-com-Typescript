bd9ac4f80c5d7b27e1f882a909cadac9a246845e
SilvaRuan16 <ruan.gustavo.soares16@gmail.com>
2025-05-02 17:31:40 -0300

    Aula 06 - Arquivo Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>Cabeçalho da aplicação</header>
         {children}
+        <footer>Rodapé da aplicação</footer>
       </body>
     </html>
   );
